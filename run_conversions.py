ada82d8ff7b04cdec445828ce6d71e8bcf070aef
Todd Clark <toddclark904@gmail.com>
2024-02-28 22:39:25 -0700

    added some comments and cleaned stuff up a little

--- run_conversions.py
+++ run_conversions.py
@@ -4,8 +4,7 @@ fulres_4326_path = r"intermediate_data/fulres_4326.tif"
 lowres_4326_path = r"intermediate_data/lowres_4326.tif"
 output_nc_path = r"output_data/output_nc.nc"
 input_tif_5070_path = r"input_data/1992_2015/1992.tif"
-if_5070_path_none_with_0 = r"intermediate_data/tif_5070_none_with_0.tif"
 template_path = r"template_data/dust_emissions_05.20210906.nc"
 scaling_factor = 0.1
 
-tif_to_nc(fulres_4326_path, lowres_4326_path, output_nc_path, input_tif_5070_path, template_path, tif_5070_path_none_with_0, scaling_factor=scaling_factor)
+tif_to_nc(fulres_4326_path, lowres_4326_path, output_nc_path, input_tif_5070_path, template_path, scaling_factor=scaling_factor)
